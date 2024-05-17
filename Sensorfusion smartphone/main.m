377ef93e0b99719ebbce448effc7a16a79dc95e2
KomaPlaton <xinzhu.niu@gmail.com>
2024-05-17 23:47:45 +0200

    Task2 completed

--- Sensorfusion smartphone/main.m
+++ Sensorfusion smartphone/main.m
@@ -1,11 +1,37 @@
+% clear;clc;
+% close all;
+% % initialize enviroment
+% startup;
+% % check IP 
+% showIP;
+% % start reciving data from App streaming
+% [xhat, meas] = filterTemplate();
+% save meas;
+% save xhat;
+
+%% Task 2
 clear;clc;
 close all;
-% initialize enviroment
-startup;
-% check IP 
-showIP;
-% start reciving data from App streaming
-[xhat, meas] = filterTemplate();
+load meas.mat;
+
+%%histogram
+meas_acc = meas.acc(:,~any(isnan(meas.acc),1));
+meas_gyr = meas.gyr(:,~any(isnan(meas.gyr),1));
+meas_mag = meas.mag(:,~any(isnan(meas.mag),1));
+
+meas_acc = meas_acc(:,400:(length(meas_acc)-1000));
+meas_gyr = meas_gyr(:,400:(length(meas_gyr)-1000));
+meas_mag = meas_mag(:,400:(length(meas_mag)-1000));
+
+figure('Position',[300 300 600 400]);
+histogram(meas_acc(1,:),'Normalization','pdf');
+figure('Position',[300 300 600 400]);
+histogram(meas_gyr(1,:),'Normalization','pdf')
+figure('Position',[300 300 600 400]);
+histogram(meas_gyr(3,:),'Normalization','pdf')
+figure('Position',[300 300 600 400]);
+histogram(meas_mag(1,:),'Normalization','pdf')
+
 %% plot
 figure('Position',[300 300 600 400]);
 plot(meas.t,meas.acc,'LineWidth',2);
@@ -28,16 +54,29 @@ legend('x','y','z')
 print('gyro.eps','-depsc');
 
 figure('Position',[300 300 600 400]);
-mag_0 = meas.mag;
-for i = 1 : length(mag_0)-1
-    if isnan(mag_0(:,i+1))
-        mag_0(:,i+1) = mag_0(:,i);
-    end
-end
-plot(meas.t,mag_0,'LineWidth',2);
+plot(meas.t,meas.mag,'LineWidth',2);
 hold on
 grid on
 title('magnetic')
 xlabel('time(s)')
 ylabel('\muT')
 print('mag.eps','-depsc');
+
+mean(meas_acc,2);
+mean(meas_gyr,2);
+mean(meas_mag,2);
+
+cov(meas_acc(1,:));
+cov(meas_acc(2,:));
+cov(meas_acc(3,:));
+cov(meas_gyr(1,:));
+cov(meas_gyr(2,:));
+cov(meas_gyr(3,:));
+cov(meas_mag(1,:));
+cov(meas_mag(2,:));
+cov(meas_mag(3,:));
+
+
+
+
+
