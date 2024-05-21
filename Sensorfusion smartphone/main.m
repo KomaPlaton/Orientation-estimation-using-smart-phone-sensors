b738a9c73686c6e03c051920c4e35abf9e6380a3
WangDian2001 <dianwang2001@gmail.com>
2024-05-21 22:55:58 +0200

    Task2 plots changed

--- Sensorfusion smartphone/main.m
+++ Sensorfusion smartphone/main.m
@@ -8,9 +8,9 @@
 % [xhat, meas] = filterTemplate();
 % save meas;
 % save xhat;
-% 
-% % Task 2
-% 
+
+%% Task 2
+
 % clear;clc;
 % close all;
 % load meas.mat;
@@ -40,97 +40,123 @@
 % mag_cov_yaxis = cov(meas_mag(2,:));
 % mag_cov_zaxis = cov(meas_mag(3,:));
 % 
-% % Plot histograms of measurements for some sensors and axes
-% figure('Position',[300 300 800 600]);
-% subplot(3,1,1);
+% % Plot histograms of measurements and signals over time
+% % For accelerometer measurements
+% figure('Position',[300 300 1200 600]);
+% subplot(3,2,1);
 % histogram(meas_acc(1,:),'Normalization','pdf');
 % hold on;
 % plot_normal_distribution(acc_mean(1), acc_cov_xaxis);
-% legend('Accelerometer X axis', 'PDF with calculated mean and covariance');
+% title('Histogram of accelerometer measurements')
+% legend('Acc X axis', 'PDF');
 % grid on;
-% subplot(3,1,2);
+% subplot(3,2,2);
+% plot(1:length(meas_acc), meas_acc(1,:),'LineWidth',0.5);
+% grid on
+% title('Accelerometer signals over time')
+% xlabel('time(s)')
+% ylabel('m/s^2')
+% subplot(3,2,3);
 % histogram(meas_acc(2,:),'Normalization','pdf');
 % hold on;
 % plot_normal_distribution(acc_mean(2), acc_cov_yaxis);
-% legend('Accelerometer Y axis', 'PDF with calculated mean and covariance');
+% legend('Acc Y axis', 'PDF');
 % grid on;
-% subplot(3,1,3);
+% subplot(3,2,4);
+% plot(1:length(meas_acc), meas_acc(2,:),'LineWidth',0.5);
+% grid on
+% xlabel('time(s)')
+% ylabel('m/s^2')
+% subplot(3,2,5);
 % histogram(meas_acc(3,:),'Normalization','pdf');
 % hold on;
 % plot_normal_distribution(acc_mean(3), acc_cov_zaxis);
-% legend('Accelerometer Z axis', 'PDF with calculated mean and covariance');
+% legend('Acc Z axis', 'PDF');
 % grid on;
-% print('Task2_histogram_acc.eps','-depsc');
+% subplot(3,2,6);
+% plot(1:length(meas_acc), meas_acc(3,:),'LineWidth',0.5);
+% grid on
+% xlabel('time(s)')
+% ylabel('m/s^2')
+% print('Task2_histogram_acc_new.eps','-depsc');
 % 
-% figure('Position',[300 300 800 600]);
-% subplot(3,1,1);
+% % For gyroscope measurements
+% figure('Position',[300 300 1200 600]);
+% subplot(3,2,1);
 % histogram(meas_gyr(1,:),'Normalization','pdf');
+% title('Histogram of gyroscope measurements')
 % hold on;
 % plot_normal_distribution(gyr_mean(1), gyr_cov_xaxis);
-% legend('Gyroscope X axis', 'PDF with calculated mean and covariance');
+% legend('Gyroscope X axis', 'PDF');
 % grid on;
-% subplot(3,1,2);
+% subplot(3,2,2);
+% plot(1:length(meas_gyr) ,meas_gyr(1,:),'LineWidth',0.5);
+% title('Gyroscope signals over time')
+% grid on
+% xlabel('time(s)')
+% ylabel('rad/s')
+% subplot(3,2,3);
 % histogram(meas_gyr(2,:),'Normalization','pdf');
 % hold on;
 % plot_normal_distribution(gyr_mean(2), gyr_cov_yaxis);
-% legend('Gyroscope Y axis', 'PDF with calculated mean and covariance');
+% legend('Gyroscope Y axis', 'PDF');
 % grid on;
-% subplot(3,1,3);
+% subplot(3,2,4);
+% plot(1:length(meas_gyr) ,meas_gyr(2,:),'LineWidth',0.5);
+% grid on
+% xlabel('time(s)')
+% ylabel('rad/s')
+% subplot(3,2,5);
 % histogram(meas_gyr(3,:),'Normalization','pdf');
 % hold on;
 % plot_normal_distribution(gyr_mean(3), gyr_cov_zaxis);
-% legend('Gyroscope Z axis', 'PDF with calculated mean and covariance');
+% legend('Gyroscope Z axis', 'PDF');
 % grid on;
-% print('Task2_histogram_gyr.eps','-depsc');
+% subplot(3,2,6);
+% plot(1:length(meas_gyr) ,meas_gyr(3,:),'LineWidth',0.5);
+% grid on
+% xlabel('time(s)')
+% ylabel('rad/s')
+% print('Task2_histogram_gyr_new.eps','-depsc');
 % 
-% figure('Position',[300 300 800 600]);
-% subplot(3,1,1);
+% % For magnetometer measurements
+% figure('Position',[300 300 1200 600]);
+% subplot(3,2,1);
 % histogram(meas_mag(1,:),'Normalization','pdf');
+% title('Histogram of magnetometer measurements')
 % hold on;
 % plot_normal_distribution(mag_mean(1), mag_cov_xaxis);
-% legend('Magnetometer X axis', 'PDF with calculated mean and covariance');
+% legend('Magnetometer X axis', 'PDF');
 % grid on;
-% subplot(3,1,2);
+% subplot(3,2,2);
+% plot(1:length(meas_mag),meas_mag(1,:),'LineWidth',0.5);
+% title('Magnetometer signals over time')
+% grid on
+% xlabel('time(s)')
+% ylabel('\muT')
+% subplot(3,2,3);
 % histogram(meas_mag(2,:),'Normalization','pdf');
 % hold on;
 % plot_normal_distribution(mag_mean(2), mag_cov_yaxis);
-% legend('Magnetometer Y axis', 'PDF with calculated mean and covariance');
+% legend('Magnetometer Y axis', 'PDF');
 % grid on;
-% subplot(3,1,3);
+% subplot(3,2,4);
+% plot(1:length(meas_mag),meas_mag(2,:),'LineWidth',0.5);
+% grid on
+% xlabel('time(s)')
+% ylabel('\muT')
+% subplot(3,2,5);
 % histogram(meas_mag(3,:),'Normalization','pdf');
 % hold on;
 % plot_normal_distribution(mag_mean(3), mag_cov_zaxis);
-% legend('Magnetometer Z axis', 'PDF with calculated mean and covariance');
+% legend('Magnetometer Z axis', 'PDF');
 % grid on;
-% print('Task2_histogram_mag.eps','-depsc');
-% 
-% % Plot signals over time for the Accelerometer
-% figure('Position',[300 300 800 600]);
-% plot(meas.t,meas.acc,'LineWidth',2);
+% subplot(3,2,6);
+% plot(1:length(meas_mag),meas_mag(3,:),'LineWidth',0.5);
 % grid on
-% title('acc')
-% xlabel('time(s)')
-% ylabel('m/s^2')
-% legend('x','y','z')
-% print('acc.eps','-depsc');
-% % Plot signals over time for the Gyroscope
-% figure('Position',[300 300 800 600]);
-% plot(meas.t,meas.gyr,'LineWidth',2);
-% grid on
-% title('gyro')
-% xlabel('time(s)')
-% ylabel('rad/s')
-% legend('x','y','z')
-% print('gyro.eps','-depsc');
-% % Plot signals over time for the Magnetometer
-% figure('Position',[300 300 800 600]);
-% plot(meas.t,meas.mag,'LineWidth',2);
-% grid on
-% title('magnetic')
 % xlabel('time(s)')
 % ylabel('\muT')
-% legend('x','y','z')
-% print('mag.eps','-depsc');
+% print('Task2_histogram_mag.eps','-depsc');
 
 % %% Task 5&6&7&8&9&10&11&11
 % clear;clc;
@@ -162,6 +188,7 @@
 %     end
 % end
 % %yaw pitch roll
+
 %% Task 12 
 clear;clc;
 close all;
