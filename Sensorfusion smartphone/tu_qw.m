fee0f34894833ea1cb4d0faea8f2635b8a8028f3
WangDian2001 <dianwang2001@gmail.com>
2024-05-21 23:49:53 +0200

    Add comment to tu_qw

--- Sensorfusion smartphone/tu_qw.m
+++ Sensorfusion smartphone/tu_qw.m
@@ -1,9 +1,12 @@
 function[x, P] = tu_qw(x, P, omega, T, Rw)
+% tu_qw: Time update function for gyroscope measurements
 
-F = eye(size(x,1))+(T/2)*Somega(omega);
-G = (T/2)*Sq(x);
+% Write out the expression for F and G
+F = eye(size(x,1)) + (T/2) * Somega(omega);
+G = (T/2) * Sq(x);
 
+% Apply the EKF prediction step
 x = F * x;
-P = F * P * F' + G * Rw *G';
+P = F * P * F' + G * Rw * G';
 
 end
